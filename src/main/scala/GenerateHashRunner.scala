9a9429b754257bd4ef9e3dbd289f0c78d6dd9f28
sourabhshrivastava748 <sourabh.shrivastava@unicommerce.com>
2023-11-10 16:25:17 +0530

    Refactor code

--- src/main/scala/GenerateHashRunner.scala
+++ src/main/scala/GenerateHashRunner.scala
@@ -55,7 +55,7 @@ object GenerateHashRunner {
 
         val dataMatch = unifillHashedDF
                 .select(col("mobile") as "unifill_mobile", col("hash") as "unifill_hash")
-                .join(meeshoDF, unifillHashedDF("unifill_hash") === meeshoDF("hash"), "inner")
+                .join(meeshoDF, col("unifill_hash") === col("hash"), "inner")
                 .select("unifill_mobile","unifill_hash")
 
         dataMatch.show(200, false)
